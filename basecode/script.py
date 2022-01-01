d917aa22c2268915833f9e0815c5c7f0c333e281
Siva <siva199708@gmail.com>
2022-01-01 04:10:00 -0500

    Commit

--- basecode/script.py
+++ basecode/script.py
@@ -4,8 +4,6 @@ from scipy.optimize import minimize
 from sklearn import svm
 import matplotlib.pyplot as plt
 
-val = 0
-
 
 def preprocess():
     """
@@ -259,6 +257,7 @@ def mlrPredict(W, data):
 
     """
     label = np.zeros((data.shape[0], 1))
+
     ##################
     # YOUR CODE HERE #
     ##################
@@ -284,6 +283,7 @@ def mlrPredict(W, data):
 Script for Logistic Regression
 """
 train_data, train_label, validation_data, validation_label, test_data, test_label = preprocess()
+
 # number of classes
 n_class = 10
 
@@ -297,27 +297,45 @@ Y = np.zeros((n_train, n_class))
 for i in range(n_class):
     Y[:, i] = (train_label == i).astype(int).ravel()
 
-# # Logistic Regression with Gradient Descent
-# W = np.zeros((n_feature + 1, n_class))
-# initialWeights = np.zeros((n_feature + 1, 1))
-# opts = {'maxiter': 1}
-# for i in range(n_class):
-#     labeli = Y[:, i].reshape(n_train, 1)
-#     args = (train_data, labeli)
-#     nn_params = minimize(blrObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
-#     W[:, i] = nn_params.x.reshape((n_feature + 1,))
-#
-# # Find the accuracy on Training Dataset
-# predicted_label = blrPredict(W, train_data)
-# print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
-#
-# # Find the accuracy on Validation Dataset
-# predicted_label = blrPredict(W, validation_data)
-# print('\n Validation set Accuracy:' + str(100 * np.mean((predicted_label == validation_label).astype(float))) + '%')
-#
-# # Find the accuracy on Testing Dataset
-# predicted_label = blrPredict(W, test_data)
-# print('\n Testing set Accuracy:' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
+# Logistic Regression with Gradient Descent
+W = np.zeros((n_feature + 1, n_class))
+initialWeights = np.zeros((n_feature + 1, 1))
+opts = {'maxiter': 100}
+for i in range(n_class):
+    labeli = Y[:, i].reshape(n_train, 1)
+    args = (train_data, labeli)
+    nn_params = minimize(blrObjFunction, initialWeights, jac=True, args=args, method='CG', options=opts)
+    W[:, i] = nn_params.x.reshape((n_feature + 1,))
+
+# Find the accuracy on Training Dataset
+predicted_label = blrPredict(W, train_data)
+print('\n Training set Accuracy:' + str(100 * np.mean((predicted_label == train_label).astype(float))) + '%')
+
+for i in range(n_class):
+    current_predicted_label = predicted_label[np.where(train_label==np.array([i]))]
+    current_train_label = train_label[np.where(train_label==np.array([i]))]
+    float_comparison = (current_predicted_label == current_train_label).astype(float)
+    print('\n Training set Accuracy for class:' + str(i) + '  ' + str(100 * np.mean(float_comparison)) + '%')
+
+# Find the accuracy on Validation Dataset
+predicted_label = blrPredict(W, validation_data)
+print('\n Validation set Accuracy:' + str(100 * np.mean((predicted_label == validation_label).astype(float))) + '%')
+
+for i in range(n_class):
+    current_predicted_label = predicted_label[np.where(validation_label==np.array([i]))]
+    current_validation_label = validation_label[np.where(validation_label==np.array([i]))]
+    float_comparison = (current_predicted_label == current_validation_label).astype(float)
+    print('\n Validation set Accuracy for class:' + str(i) + '  ' + str(100 * np.mean(float_comparison)) + '%')
+
+# Find the accuracy on Testing Dataset
+predicted_label = blrPredict(W, test_data)
+print('\n Testing set Accuracy:' + str(100 * np.mean((predicted_label == test_label).astype(float))) + '%')
+
+for i in range(n_class):
+    current_predicted_label = predicted_label[np.where(test_label==np.array([i]))]
+    current_test_label = test_label[np.where(test_label==np.array([i]))]
+    float_comparison = (current_predicted_label == current_test_label).astype(float)
+    print('\n Testing set Accuracy for class:' + str(i) + '  ' + str(100 * np.mean(float_comparison)) + '%')
 
 """
 Script for Support Vector Machine
@@ -328,14 +346,92 @@ print('\n\n--------------SVM-------------------\n\n')
 # YOUR CODE HERE #
 ##################
 
+# Randomly Selecting 1000 samples for training SVM
+index = np.random.randint(50000, size=10000)
+X_svm = train_data[index, :]
+y_svm = train_label[index, :]
 
+# Initializing Linear SVM
+svm_linear = svm.SVC(kernel='linear')
+svm_linear.fit(X_svm, y_svm.ravel())
+
+# Printing the results for linear SVM Model
+print('\n-----------------------------------------SVM with Linear Kernel-----------------------------------------\n')
+print('\n SVM Training Data Accuracy : ' + str(100 * svm_linear.score(X_svm, y_svm)) + '%')
+print('\n Training Data Accuracy : ' + str(100 * svm_linear.score(train_data, train_label)) + '%')
+print('\n Validation Data Accuracy : ' + str(100 * svm_linear.score(validation_data, validation_label)) + '%')
+print('\n Testing Data Accuracy : ' + str(100 * svm_linear.score(test_data, test_label)) + '%')
+
+# RBF with Gamma = 1
+
+svm_rbf = svm.SVC(kernel='rbf', gamma=1.0)
+svm_rbf.fit(X_svm, y_svm.ravel())
+
+print('\n-----------------------------------------SVM RBF ; Gamma = 1-----------------------------------------\n')
+print('\n SVM Training Data Accuracy : ' + str(100 * svm_rbf.score(X_svm, y_svm)) + '%')
+print('\n Training Accuracy : ' + str(100 * svm_rbf.score(train_data, train_label)) + '%')
+print('\n Validation Accuracy : ' + str(100 * svm_rbf.score(validation_data, validation_label)) + '%')
+print('\n Testing Accuracy : ' + str(100 * svm_rbf.score(test_data, test_label)) + '%')
+
+# RBF with gamma = default(0.0)
+
+svm_rbf_default_gamma = svm.SVC(kernel='rbf', gamma='auto')
+svm_rbf_default_gamma.fit(X_svm, y_svm.ravel())
+
+print(
+    '\n-----------------------------------------SVM RBF ; Gamma = default(0.0)-----------------------------------------\n')
+print('\n SVM Training Data Accuracy : ' + str(100 * svm_rbf_default_gamma.score(X_svm, y_svm)) + '%')
+print('\n Training Accuracy : ' + str(100 * svm_rbf_default_gamma.score(train_data, train_label)) + '%')
+print('\n Validation Accuracy : ' + str(100 * svm_rbf_default_gamma.score(validation_data, validation_label)) + '%')
+print('\n Testing Accuracy : ' + str(100 * svm_rbf_default_gamma.score(test_data, test_label)) + '%')
+
+# RBF with gamma = default(0.0) and Changing values of C to find the optimal one
+
+svm_Xtrain_accuracy = np.zeros(11, float)
+svm_train_accuracy = np.zeros(11, float)
+svm_validation_accuracy = np.zeros(11, float)
+svm_test_accuracy = np.zeros(11, float)
+C_values = np.array([1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
+for i, C in enumerate(C_values):
+    svm_rbf_C = svm.SVC(kernel='rbf', C=C)
+    svm_rbf_C.fit(X_svm, y_svm.ravel())
+
+    svm_Xtrain_accuracy[i] = 100 * svm_rbf_C.score(X_svm, y_svm)
+    svm_train_accuracy[i] = 100 * svm_rbf_C.score(train_data, train_label)
+    svm_validation_accuracy[i] = 100 * svm_rbf_C.score(validation_data, validation_label)
+    svm_test_accuracy[i] = 100 * svm_rbf_C.score(test_data, test_label)
+
+    print('\n-----------------------------------------SVM RBF ; Gamma = default(0.0) ; C  = ' + str(
+        C) + '-----------------------------------------\n')
+    print('\n SVM Training Data Accuracy : ' + str(svm_Xtrain_accuracy[i]) + '%')
+    print('\n Training Accuracy : ' + str(svm_train_accuracy[i]) + '%')
+    print('\n Validation Accuracy : ' + str(svm_validation_accuracy[i]) + '%')
+    print('\n Testing Accuracy : ' + str(svm_test_accuracy[i]) + '%')
+
+# Accuracy vs C Plot
+plt.plot(C_values, svm_Xtrain_accuracy, color='b')
+plt.plot(C_values, svm_train_accuracy, color='g')
+plt.plot(C_values, svm_validation_accuracy, color='r')
+plt.plot(C_values, svm_test_accuracy, color='y')
+
+plt.figure(figsize=(16, 16))
+plt.title('Accuracy vs C', fontweight='bold')
+plt.xlabel('C', fontweight='bold')
+plt.ylabel('Accuracy', fontweight='bold')
+plt.xticks(np.array([1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
+plt.yticks(np.arange(95, 100, step=0.1))
+plt.grid(True)
+
+plt.legend(['SVM Training Data Accuracy', 'Training Accuracy', 'Validation Accuracy', 'Test Accuracy'])
+plt.show()
 """
 Script for Extra Credit Part
 """
 # FOR EXTRA CREDIT ONLY
 W_b = np.zeros((n_feature + 1, n_class))
 initialWeights_b = np.zeros((n_feature + 1, n_class))
-opts_b = {'maxiter': 8}
+opts_b = {'maxiter': 100}
 
 args_b = (train_data, Y)
 nn_params = minimize(mlrObjFunction, initialWeights_b, jac=True, args=args_b, method='CG', options=opts_b)
